fdff633fa4dc202ec260d022d3e7b0c679fb9298
Luca-Keller <Andreasks56@gmail.com>
2025-06-19 10:31:51 +0200

    Boostrap installiert

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,27 @@ import { RouterOutlet } from '@angular/router';
   imports: [RouterOutlet],
   template: `
     <h1>Welcome to {{title}}!</h1>
+    <div class="container text-center">
+  <div class="row">
+    <div class="col">
+      Column
+    </div>
+    <div class="col">
+      Column
+    </div>
+    <div class="col">
+      Column
+    </div>
+  </div>
+</div>
+<div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
 
     <router-outlet />
   `,
---
+++ src/app/components/filtertabelle/filtertabelle.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filtertabelle4',
+  imports: [],
+  template: `
+    <p>
+      filtertabelle4 works!
+    </p>
+  `,
+  styles: ``
+})
+export class Filtertabelle4Component {
+
+}
