c1879deb83b708c4dfea909d58774d1246ff117d
Luca-Keller <Andreasks56@gmail.com>
2025-06-24 20:09:51 +0200

    Page für Filtertabelle erstellt

---
+++ src/app/pages/tabelle-page/tabelle-page.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tabelle-page',
+  imports: [],
+  template: `
+    <p>
+      tabelle-page works!
+    </p>
+  `,
+  styles: ``
+})
+export class TabellePageComponent {
+
+}
