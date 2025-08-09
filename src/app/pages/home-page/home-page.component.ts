edad7208b40bd7746e867821afe7e81c577680e7
Luca-Keller <Andreasks56@gmail.com>
2025-08-09 09:28:49 +0200

    Erster Wurf einer Home page

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -4,10 +4,27 @@ import { Component } from '@angular/core';
   selector: 'app-home-page',
   imports: [],
   template: `
-    <p>
-      home-page works!
+    <div class="rounded bg-light-blue container  p-3">
+      <div class="row">
+        <div class="col">
+          <h1>Home Page</h1>
+          <p>Welcome to the home page!</p>
+          <p>This is a simple Angular application.</p>
+          <p>Feel free to explore the features and functionalities.</p>
+        </div>
+        <div class="col">
+          <img src="Regenwald.png" class="img-fluid rounded" alt="Regenwald" />
+        </div>
+      </div>
+    </div>
+
+    <p class="m-4">
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde voluptate
+      minima, molestias saepe eligendi mollitia vero. Natus labore delectus in,
+      fugiat sed, perspiciatis itaque tenetur odit sint veritatis, beatae
+      cumque.
     </p>
   `,
-  styles: ``
+  styles: ``,
 })
 export class HomePageComponent {}
