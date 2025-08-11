54daa45cfa62005c46eb85287344f1704294057a
Luca-Keller <Andreasks56@gmail.com>
2025-08-11 07:58:05 +0200

    Seiten

--- src/app/pages/about-page/about-page.component.ts
+++ src/app/pages/about-page/about-page.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 
-
 @Component({
   selector: 'app-about-page',
   imports: [],
   template: `
-    <p>
-      about-page works!
-    </p>
+    <div class="container-fluid p-3">
+      <p>
+        Diese Seite hat keine Funktionalität, sondern dient lediglich als
+        Beispiel für das Navigieren über den Header. Nachfolgend der Demtext,
+        damit es nicht so leer aussieht ;-)
+        <br />
+        <br />
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, rerum
+        possimus inventore ratione deleniti ea aut accusantium esse quis quaerat
+        dolorem quidem corporis nostrum molestias? Assumenda architecto quidem
+        voluptates officiis!
+      </p>
+    </div>
   `,
-  styles: ``
+  styles: ``,
 })
 export class AboutPageComponent {}
