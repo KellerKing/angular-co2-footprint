ebdecdc482ddc2e66444b20cbf2425bdf786a624
Luca-Keller <Andreasks56@gmail.com>
2026-03-15 09:07:21 +0100

    Sicherung

--- src/app/layout/footer/dialog-einstellungen/dialog-einstellungen.ts
+++ src/app/layout/footer/dialog-einstellungen/dialog-einstellungen.ts
@@ -1,11 +1,11 @@
 import { Component, effect, inject, signal } from '@angular/core';
-import { form } from '@angular/forms/signals';
+import { form,  FormField } from '@angular/forms/signals';
 import { MatDialogContent, MatDialogActions, MatDialogClose, MatDialogTitle } from '@angular/material/dialog';
 import { SettingsService } from '../../../service/settings.service';
 
 @Component({
   selector: 'app-dialog-einstellungen',
-  imports: [MatDialogContent, MatDialogActions, MatDialogClose, MatDialogTitle],
+  imports: [MatDialogContent, MatDialogActions, MatDialogClose, MatDialogTitle, FormField],
   templateUrl: './dialog-einstellungen.html',
   styleUrl: './dialog-einstellungen.css',
 })
