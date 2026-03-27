65f0ec283e1531247e57c5054ad24d37493828c1
Luca-Keller <Andreasks56@gmail.com>
2026-03-27 08:13:39 +0100

    Unit Tests für Tabellenlogik

--- src/app/page/page-tabelle/components/tabelle-component/tabelle-component.spec.ts
+++ src/app/page/page-tabelle/components/tabelle-component/tabelle-component.spec.ts
@@ -22,7 +22,7 @@ describe('TabelleComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should reset page size to 25 when data grows after a small result set', async () => {
+  it("PageSize bleibt unverändert wenn Daten aktualisiert werden und neue Ergebnismenge nicht kleiner als der gesetzte Wert ist", async () => {
     fixture.componentRef.setInput('viewModels', createViewModels(5));
     fixture.detectChanges();
     await fixture.whenStable();
@@ -33,7 +33,50 @@ describe('TabelleComponent', () => {
     fixture.detectChanges();
     await fixture.whenStable();
 
-    expect(component.m_DataSource.paginator?.pageSize).toBe(25);
+    expect(component.m_DataSource.paginator?.pageSize).toBe(5);
+  });
+
+
+  it("PageSize wird angepasst wenn Daten aktualisiert werden und neue Ergebnismenge kleiner als der gesetzte Wert ist. Dann wird auf den definierten Default gesetzt", async () => {
+    fixture.componentRef.setInput('viewModels', createViewModels(50));
+    fixture.detectChanges();
+    await fixture.whenStable();
+    expect(component.m_DataSource.paginator?.pageSize).toBe(TabelleComponent.MAX_INITIAL_PAGE_SIZE_BEI_NEU_LADEN);
+    
+    component.m_DataSource.paginator!.pageSize = 50;
+    fixture.detectChanges();
+    await fixture.whenStable();
+    expect(component.m_DataSource.paginator?.pageSize).toBe(50);
+
+    fixture.componentRef.setInput('viewModels', createViewModels(30));
+    fixture.detectChanges();
+    await fixture.whenStable();
+
+    expect(component.m_DataSource.paginator?.pageSize).toBe(TabelleComponent.MAX_INITIAL_PAGE_SIZE_BEI_NEU_LADEN);
+  });
+
+
+    it("Initial ist PageSize nicht größer als der max Wert definiert in init size obwohl Mehr Daten da sind", async () => {
+    fixture.componentRef.setInput('viewModels', createViewModels(50));
+    fixture.detectChanges();
+    await fixture.whenStable();
+
+    expect(component.m_DataSource.paginator?.pageSize).toBe(TabelleComponent.MAX_INITIAL_PAGE_SIZE_BEI_NEU_LADEN);
+  });
+
+
+    it("Initiale PageSize wird auf 0 gesetzt wenn keine Daten und wenn dann Ergebnismenge kleiner als der Default ist, wird auf diesen Wert gesetzt auch wenn er nicht in den definierten Werten ist.", async () => {
+    fixture.componentRef.setInput('viewModels', createViewModels(0));
+    fixture.detectChanges();
+    await fixture.whenStable();
+
+    expect(component.m_DataSource.paginator?.pageSize).toBe(0);
+
+    fixture.componentRef.setInput('viewModels', createViewModels(6));
+    fixture.detectChanges();
+    await fixture.whenStable();
+
+    expect(component.m_DataSource.paginator?.pageSize).toBe(6);
   });
 });
 
